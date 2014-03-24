300752806d9c1389dc6df537296483d9b52ec35e
Chris Gianelloni <chris@continuuity.com>
2014-03-24 12:07:43 -0700

    Initial loom-standalone project

---
+++ config/projects/loom-standalone.rb
@@ -0,0 +1,25 @@
+
+name "loom-standalone"
+maintainer "Continuuity"
+homepage "http://github.com/continuuity/loom"
+
+replaces        "loom"
+install_path    "/opt/loom"
+#build_version   Omnibus::BuildVersion.new.semver
+build_version "0.9.5"
+build_iteration 101
+
+# creates required build directories
+dependency "preparation"
+
+# loom-provisioner dependencies/components
+# dependency "somedep"
+dependency "loom-provisioner"
+dependency "loom-server"
+dependency "loom-ui"
+
+# version manifest file
+dependency "version-manifest"
+
+exclude "\.git*"
+exclude "bundler\/git"
