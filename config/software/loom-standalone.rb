9e02985979173afd4f3f15af6e6f1429b60337db
Chris Gianelloni <chris@continuuity.com>
2014-03-24 18:44:22 -0700

    Add software for loom-standalone to pull in start script

---
+++ config/software/loom-standalone.rb
@@ -0,0 +1,13 @@
+name "loom-standalone"
+default_version "develop"
+
+source :git => "git://github.com/continuuity/loom.git"
+relative_path 'standalone'
+
+build do
+  command "mkdir -p #{install_dir}/bin"
+  command "cp -fpPR bin/loom.sh #{install_dir}/bin"
+  %w[ provisioner server ui ].each do |c|
+    command "ln -sf #{install_dir}/#{c}/bin/loom-#{c}.sh #{install_dir}/bin/loom-#{c}.sh"
+  end
+end
