c34dad8cf24a2c723c9ffde0700b17791aba92a2
Chris Gianelloni <chris@continuuity.com>
2014-04-08 13:58:19 -0700

    Update UI build to fix path to node

--- config/software/loom-ui.rb
+++ config/software/loom-ui.rb
@@ -8,7 +8,7 @@ relative_path 'ui'
 
 build do
   command "mkdir -p #{install_dir}/ui/bin"
-  command "cp -fpPR bin/loom-ui.sh #{install_dir}/ui/bin"
+  command "sed -e 's:ui/embedded/bin:embedded/bin:g' bin/loom-ui.sh > #{install_dir}/ui/bin/loom-ui.sh"
   command "mkdir -p #{install_dir}/ui/etc/logrotate.d"
   command "cp -f /var/cache/omnibus/src/node*/LICENSE #{install_dir}/ui/LICENSE.node"
   command "cp -f distribution/etc/logrotate.d/loom-ui #{install_dir}/ui/etc/logrotate.d"
