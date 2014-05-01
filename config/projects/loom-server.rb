7d53bd2f3bf7bc9c7925e40eed11dc4249d06639
Chris Gianelloni <chris@continuuity.com>
2014-04-30 18:14:12 -0700

    Auto-updated using rubocop -a

--- config/projects/loom-server.rb
+++ config/projects/loom-server.rb
@@ -1,23 +1,23 @@
 
-name "loom-server"
-maintainer "Continuuity"
-homepage "http://github.com/continuuity/loom"
+name 'loom-server'
+maintainer 'Continuuity'
+homepage 'http://github.com/continuuity/loom'
 
-replaces        "loom-server"
-install_path    "/opt/loom"
-#build_version   Omnibus::BuildVersion.new.semver
-build_version "0.9.6"
+replaces 'loom-server'
+install_path '/opt/loom'
+# build_version   Omnibus::BuildVersion.new.semver
+build_version '0.9.6'
 build_iteration 1
 
 # creates required build directories
-dependency "preparation"
+dependency 'preparation'
 
 # loom-provisioner dependencies/components
 # dependency "somedep"
-dependency "loom-server"
+dependency 'loom-server'
 
 # version manifest file
-#dependency "version-manifest"
+# dependency "version-manifest"
 
 exclude "\.git*"
 exclude "bundler\/git"
--- config/software/loom-server.rb
+++ config/software/loom-server.rb
@@ -1,7 +1,7 @@
-name "loom-server"
-default_version "develop"
+name 'loom-server'
+default_version 'develop'
 
-source :git => "git://github.com/continuuity/loom.git"
+source :git => 'git://github.com/continuuity/loom.git'
 relative_path 'server'
 
 build do
@@ -14,6 +14,6 @@ build do
   command "sed -e 's/APP_NAME/loom-server/g' -e 's/SVC_NAME/server/g' bin/loom-service > #{install_dir}/server/bin/init-loom-server"
   command "chmod +x #{install_dir}/server/bin/*"
   command "mkdir -p #{install_dir}/server/lib"
-  command "cd server && PATH=/usr/local/maven-3.1.1/bin:$PATH mvn clean package assembly:single -DskipTests=true"
+  command 'cd server && PATH=/usr/local/maven-3.1.1/bin:$PATH mvn clean package assembly:single -DskipTests=true'
   command "cp -fpPR server/target/loom-*jar-with-dependencies.jar #{install_dir}/server/lib"
 end
