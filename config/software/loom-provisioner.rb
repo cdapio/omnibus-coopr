a3cd573e4480a181226026f68d28e0764794d7ae
Chris Gianelloni <chris@continuuity.com>
2014-08-19 10:57:56 -0700

    Comment out nokogiri and dependencies, since we shouldn't need them, and add our dependencies for new provisioner code

--- config/software/loom-provisioner.rb
+++ config/software/loom-provisioner.rb
@@ -4,19 +4,19 @@ default_version 'develop'
 dependency 'ruby'
 dependency 'rubygems'
 dependency 'bundler'
-dependency 'zlib'
-dependency 'libxml2'
-dependency 'libxslt'
-dependency 'libffi'
-dependency 'nokogiri'
+# dependency 'zlib'
+# dependency 'libxml2'
+# dependency 'libxslt'
+# dependency 'libffi'
+# dependency 'nokogiri'
 
 source :git => 'git://github.com/continuuity/loom.git'
 relative_path 'provisioner'
 
 build do
-  gem 'install knife-joyent --no-rdoc --no-ri --version 0.4.8'
-  gem 'install knife-rackspace --no-rdoc --no-ri --version 0.9.1'
-  gem 'install knife-openstack --no-rdoc --no-ri --version 0.8.1'
+  gem 'install fog --no-rdoc --no-ri --version 1.21.0'
+  gem 'install sinatra --no-rdoc --no-ri --version 1.4.5'
+  gem 'install thin --no-rdoc --no-ri --version 1.6.2'
   command "mkdir -p #{install_dir}/provisioner/bin"
   command "sed -e 's:provisioner/embedded/bin:embedded/bin:g' bin/loom-provisioner.sh > #{install_dir}/provisioner/bin/loom-provisioner.sh"
   command "cp -fpPR provisioner #{install_dir}"
@@ -27,7 +27,5 @@ build do
   command "find #{install_dir} -type f -name .gitkeep | xargs rm -f"
   # Joyent ships with bad permissions, allow everyone to read everything
   command "chmod ugo+r #{install_dir}/embedded"
-  gem 'uninstall fog -v1.20.0'
-  gem 'uninstall nokogiri -v1.6.1'
   gem 'uninstall -Ix rdoc'
 end
