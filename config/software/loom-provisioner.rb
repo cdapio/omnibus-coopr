9d557c563f6d8bb01615a0c8ef3ac2a4ddc729a8
Chris Gianelloni <chris@continuuity.com>
2014-09-05 19:42:58 -0700

    Don't use project variable, since we reuse software with loom-standalone project

--- config/software/loom-provisioner.rb
+++ config/software/loom-provisioner.rb
@@ -1,4 +1,4 @@
-name project
+name loom-provisioner
 default_version 'develop'
 
 dependency 'ruby'
@@ -14,10 +14,10 @@ build do
   gem 'install thin --no-rdoc --no-ri --version 1.6.2'
   gem 'install rest_client --no-rdoc --no-ri --version 1.7.3'
   command "cp -fpPR provisioner #{install_dir}"
-  command "sed -e 's/APP_NAME/#{project}/g' -e 's/SVC_NAME/provisioner/g' bin/loom-service > #{install_dir}/provisioner/bin/init-#{project}"
+  command "sed -e 's/APP_NAME/loom-provisioner/g' -e 's/SVC_NAME/provisioner/g' bin/loom-service > #{install_dir}/provisioner/bin/init-loom-provisioner"
   command "chmod +x #{install_dir}/provisioner/bin/*"
   mkdir "#{install_dir}/provisioner/etc/logrotate.d"
-  copy "#{project_dir}/provisioner/distribution/etc/logrotate.d/#{project}", "#{install_dir}/provisioner/etc/logrotate.d"
+  copy "#{project_dir}/provisioner/distribution/etc/logrotate.d/loom-provisioner", "#{install_dir}/provisioner/etc/logrotate.d"
   command "find #{install_dir} -type f -name .gitkeep | xargs rm -f"
   gem 'uninstall -Ix rdoc'
 end
