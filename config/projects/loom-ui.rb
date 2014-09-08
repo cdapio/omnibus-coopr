6568b87e4bdf4c0fb02bd6b477c5f1a54f38d838
Chris Gianelloni <chris@continuuity.com>
2014-09-08 15:15:29 -0700

    Change install_dir to subdirectories

--- config/projects/loom-ui.rb
+++ config/projects/loom-ui.rb
@@ -2,7 +2,7 @@ name 'loom-ui'
 maintainer 'Continuuity'
 homepage 'http://github.com/continuuity/loom'
 
-install_path '/opt/loom'
+install_dir '/opt/loom/ui'
 build_version   Omnibus::BuildVersion.semver
 # build_version '0.9.7'
 build_iteration 1
