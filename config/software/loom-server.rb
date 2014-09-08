cb1fb5e5f4e1b93b541d71f0b36c4c19f8f6dafc
Chris Gianelloni <chris@continuuity.com>
2014-09-08 16:57:14 -0700

    Update paths

--- config/software/loom-server.rb
+++ config/software/loom-server.rb
@@ -6,16 +6,16 @@ relative_path 'server'
 
 build do
   mkdir "#{install_dir}/bin"
-  copy "#{project_dir}/bin/*", "#{install_dir}/bin"
-  command "cp -fpPR docs/licenses config #{install_dir}"
+  copy "#{project_dir}/server/bin/*", "#{install_dir}/bin"
+  command "cp -fpPR #{project_dir}/docs/licenses #{project_dir}/config #{install_dir}"
   mkdir "#{install_dir}/etc/logrotate.d"
-  copy "#{project_dir}/distribution/etc/logrotate.d/loom-server", "#{install_dir}/etc/logrotate.d"
+  copy "#{project_dir}/server/distribution/etc/logrotate.d/loom-server", "#{install_dir}/etc/logrotate.d"
   mkdir "#{install_dir}/conf"
-  copy "#{project_dir}/distribution/etc/loom/conf.dist/*", "#{install_dir}/conf"
+  copy "#{project_dir}/server/distribution/etc/loom/conf.dist/*", "#{install_dir}/conf"
   # command "sed -e 's/APP_NAME/loom-server/g' -e 's/SVC_NAME/server/g' bin/loom-service > #{install_dir}/server/bin/init-loom-server"
   command "chmod +x #{install_dir}/bin/*"
   mkdir "#{install_dir}/lib"
-  command 'PATH=/usr/local/maven-3.1.1/bin:$PATH mvn clean package assembly:single -DskipTests=true'
-  copy "target/loom-*jar-with-dependencies.jar", "#{install_dir}/lib"
+  command 'cd server; PATH=/usr/local/maven-3.1.1/bin:$PATH mvn clean package assembly:single -DskipTests=true'
+  copy "server/target/loom-*jar-with-dependencies.jar", "#{install_dir}/lib"
   command "find #{install_dir} -type f -name .gitkeep | xargs rm -f"
 end
