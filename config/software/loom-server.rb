9a17c2c2e7d887f436b8ba270d521a26cd44c123
Chris Gianelloni <chris@continuuity.com>
2014-09-08 12:08:31 -0700

    Copy takes two arguments, not one

--- config/software/loom-server.rb
+++ config/software/loom-server.rb
@@ -6,17 +6,17 @@ relative_path 'server'
 
 build do
   mkdir "#{install_dir}/server/bin"
-  copy "bin/loom-server.sh #{install_dir}/server/bin"
-  copy "bin/loom-0.9.7-upgrade.rb #{install_dir}/server/bin"
+  copy "bin/loom-server.sh", "#{install_dir}/server/bin"
+  copy "bin/loom-0.9.7-upgrade.rb", "#{install_dir}/server/bin"
   command "cp -fpPR docs/licenses config #{install_dir}/server"
   mkdir "#{install_dir}/server/etc/logrotate.d"
-  copy "server/distribution/etc/logrotate.d/loom-server #{install_dir}/server/etc/logrotate.d"
+  copy "server/distribution/etc/logrotate.d/loom-server", "#{install_dir}/server/etc/logrotate.d"
   mkdir "#{install_dir}/server/conf"
-  copy "server/distribution/etc/loom/conf.dist/* #{install_dir}/server/conf"
+  copy "server/distribution/etc/loom/conf.dist/*", "#{install_dir}/server/conf"
   command "sed -e 's/APP_NAME/loom-server/g' -e 's/SVC_NAME/server/g' bin/loom-service > #{install_dir}/server/bin/init-loom-server"
   command "chmod +x #{install_dir}/server/bin/*"
   mkdir "#{install_dir}/server/lib"
   command 'cd server && PATH=/usr/local/maven-3.1.1/bin:$PATH mvn clean package assembly:single -DskipTests=true'
-  copy "server/target/loom-*jar-with-dependencies.jar #{install_dir}/server/lib"
+  copy "server/target/loom-*jar-with-dependencies.jar", "#{install_dir}/server/lib"
   command "find #{install_dir} -type f -name .gitkeep | xargs rm -f"
 end
