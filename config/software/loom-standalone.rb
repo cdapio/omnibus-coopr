c3fc3b550558ea140deda879c142d6c3d54ce583
Chris Gianelloni <chris@continuuity.com>
2014-09-15 13:12:39 -0700

    Do not copy upgrade file, since it is not used anymore

--- config/software/loom-standalone.rb
+++ config/software/loom-standalone.rb
@@ -6,10 +6,9 @@ relative_path 'standalone'
 
 build do
   mkdir "#{install_dir}/bin"
-  copy "server/bin/loom-0.9.7-upgrade.rb", "#{install_dir}/bin"
   command "sed -e 's#_NODE:-node#_NODE:-/opt/loom/embedded/bin/node#g' -e 's#_RUBY:-ruby#_RUBY:-/opt/loom/embedded/bin/ruby#g' standalone/bin/loom.sh > #{install_dir}/bin/loom.sh"
   %w(provisioner server ui).each do |c|
-    link "#{install_dir}/#{c}/bin/loom-#{c}.sh", "#{install_dir}/bin/loom-#{c}.sh"
+    link "#{install_dir}/#{c}/bin/#{c}.sh", "#{install_dir}/bin/#{c}.sh"
   end
   command "chmod +x #{install_dir}/bin/*"
 end
