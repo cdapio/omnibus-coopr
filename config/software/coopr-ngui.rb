07eeb83d9808e6b37a0fce5db9faaf1402a6f237
Chris Gianelloni <chris@continuuity.com>
2014-09-15 14:44:00 -0700

    Look for bower/gulp under ngui

--- config/software/coopr-ngui.rb
+++ config/software/coopr-ngui.rb
@@ -14,10 +14,11 @@ build do
   copy "#{project_dir}/ngui/bin/*", "#{install_dir}/bin"
   copy '/var/cache/omnibus/src/node-v0.10.26/LICENSE', "#{install_dir}/LICENSE.node"
   command "chmod +x #{install_dir}/bin/*"
+
   command "cd #{project_dir}/ngui; #{npm} install bower"
   command "cd #{project_dir}/ngui; #{npm} install"
-  command "cd #{project_dir}/ngui; #{node} #{project_dir}/node_modules/bower/bin/bower install --config.interactive=false"
-  command "cd #{project_dir}/ngui; #{node} #{project_dir}/node_modules/gulp/bin/gulp.js build minify"
+  command "cd #{project_dir}/ngui; #{node} #{project_dir}/ngui/node_modules/bower/bin/bower install --config.interactive=false"
+  command "cd #{project_dir}/ngui; #{node} #{project_dir}/ngui/node_modules/gulp/bin/gulp.js build minify"
 
   copy "#{project_dir}/ngui/dist*", "#{install_dir}"
   copy "#{project_dir}/ngui/server.js", "#{install_dir}"
