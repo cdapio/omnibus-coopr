f84f59a7e46e5e84c2a18d49441638fb533d3094
Chris Gianelloni <chris@continuuity.com>
2014-09-15 14:18:05 -0700

    Switch to correct subdirectory

--- config/software/coopr-ngui.rb
+++ config/software/coopr-ngui.rb
@@ -14,14 +14,14 @@ build do
   copy "#{project_dir}/ngui/bin/*", "#{install_dir}/bin"
   copy '/var/cache/omnibus/src/node-v0.10.26/LICENSE', "#{install_dir}/LICENSE.node"
   command "chmod +x #{install_dir}/bin/*"
-  command "cd #{project_dir}; #{npm} install bower"
-  command "cd #{project_dir}; #{npm} install"
-  command "cd #{project_dir}; #{node} #{project_dir}/node_modules/bower/bin/bower install --config.interactive=false"
-  command "cd #{project_dir}; #{node} #{project_dir}/node_modules/gulp/bin/gulp.js build minify"
+  command "cd #{project_dir}/ngui; #{npm} install bower"
+  command "cd #{project_dir}/ngui; #{npm} install"
+  command "cd #{project_dir}/ngui; #{node} #{project_dir}/node_modules/bower/bin/bower install --config.interactive=false"
+  command "cd #{project_dir}/ngui; #{node} #{project_dir}/node_modules/gulp/bin/gulp.js build minify"
 
   copy "#{project_dir}/ngui/dist*", "#{install_dir}"
-  copy "#{project_dir}/server.js", "#{install_dir}"
-  copy "#{project_dir}/package.json", "#{install_dir}"
+  copy "#{project_dir}/ngui/server.js", "#{install_dir}"
+  copy "#{project_dir}/ngui/package.json", "#{install_dir}"
 
   command "cd #{install_dir}; #{npm} install --production"
 
