c8ba21c29641ab9ec896ee26f6b9e374a8a661de
Chris Gianelloni <chris@continuuity.com>
2014-09-15 14:25:32 -0700

    Add ngui to standalone symlink loop

--- config/software/coopr-standalone.rb
+++ config/software/coopr-standalone.rb
@@ -7,7 +7,7 @@ relative_path 'standalone'
 build do
   mkdir "#{install_dir}/bin"
   command "sed -e 's#_NODE:-node#_NODE:-/opt/coopr/embedded/bin/node#g' -e 's#_RUBY:-ruby#_RUBY:-/opt/coopr/embedded/bin/ruby#g' standalone/bin/coopr.sh > #{install_dir}/bin/coopr.sh"
-  %w(provisioner server ui).each do |c|
+  %w(provisioner server ngui ui).each do |c|
     link "#{install_dir}/#{c}/bin/#{c}.sh", "#{install_dir}/bin/#{c}.sh"
   end
   command "chmod +x #{install_dir}/bin/*"
