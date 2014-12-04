8f8b6207abf53450661899f1642ff60d8f1bd575
Chris Gianelloni <chris@cask.co>
2014-12-04 10:26:39 -0800

    Rename ngui to ui

--- config/software/coopr-ui.rb
+++ config/software/coopr-ui.rb
@@ -1,29 +1,29 @@
-name 'coopr-ngui'
+name 'coopr-ui'
 default_version 'develop'
 
 dependency 'nodejs'
 
 source :git => 'git://github.com/caskdata/coopr.git'
-relative_path 'coopr-ngui'
+relative_path 'coopr-ui'
 
 npm = "#{install_dir}/embedded/bin/npm"
 node = "#{install_dir}/embedded/bin/node"
 
 build do
   mkdir "#{install_dir}/bin"
-  copy "#{project_dir}/coopr-ngui/bin/*", "#{install_dir}/bin"
+  copy "#{project_dir}/coopr-ui/bin/*", "#{install_dir}/bin"
   copy '/var/cache/omnibus/src/node-v0.10.26/LICENSE', "#{install_dir}/LICENSE.node"
   command "chmod +x #{install_dir}/bin/*"
 
-  command "cd #{project_dir}/coopr-ngui; #{npm} install bower"
-  command "cd #{project_dir}/coopr-ngui; #{npm} install"
-  command "cd #{project_dir}/coopr-ngui; #{node} #{project_dir}/coopr-ngui/node_modules/bower/bin/bower install --config.interactive=false"
-  command "cd #{project_dir}/coopr-ngui; #{node} #{project_dir}/coopr-ngui/node_modules/gulp/bin/gulp.js clean"
-  command "cd #{project_dir}/coopr-ngui; #{node} #{project_dir}/coopr-ngui/node_modules/gulp/bin/gulp.js distribute"
+  command "cd #{project_dir}/coopr-ui; #{npm} install bower"
+  command "cd #{project_dir}/coopr-ui; #{npm} install"
+  command "cd #{project_dir}/coopr-ui; #{node} #{project_dir}/coopr-ui/node_modules/bower/bin/bower install --config.interactive=false"
+  command "cd #{project_dir}/coopr-ui; #{node} #{project_dir}/coopr-ui/node_modules/gulp/bin/gulp.js clean"
+  command "cd #{project_dir}/coopr-ui; #{node} #{project_dir}/coopr-ui/node_modules/gulp/bin/gulp.js distribute"
 
-  copy "#{project_dir}/coopr-ngui/dist*", "#{install_dir}"
-  copy "#{project_dir}/coopr-ngui/server.js", "#{install_dir}"
-  copy "#{project_dir}/coopr-ngui/package.json", "#{install_dir}"
+  copy "#{project_dir}/coopr-ui/dist*", "#{install_dir}"
+  copy "#{project_dir}/coopr-ui/server.js", "#{install_dir}"
+  copy "#{project_dir}/coopr-ui/package.json", "#{install_dir}"
 
   command "cd #{install_dir}; #{npm} install --production"
 
