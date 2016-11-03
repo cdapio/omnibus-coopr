f61e1a3304f7555ee319d13d8ea5e9ffdd02f065
Chris Gianelloni <chris@cask.co>
2016-11-03 12:44:00 -0400

    Create Dockerfiles for building in Docker (#62)

---
+++ build-docker.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+COOPR_DISTRIBUTIONS=${COOPR_DISTRIBUTIONS:-centos ubuntu}
+
+die() { echo "ERROR: ${@}"; exit 1; }
+
+__clean() {
+  rm -rf target
+  for __distro in ${COOPR_DISTRIBUTIONS}; do
+    __containers=$(docker ps -a | grep omnibus-coopr-${__distro} | awk '{print $1}' 2>/dev/null)
+    if [[ -n ${__containers} ]]; then
+      for __id in ${__containers}; do
+        docker stop ${__id} 2>/dev/null
+        docker rm ${__id} 2>/dev/null
+      done
+    fi
+    __images=$(docker images | grep omnibus-coopr-${__distro} | awk '{print $3' | sort -u 2>/dev/null)
+    if [[ -n ${__images} ]]; then
+      for __img in ${__images}; do
+        docker rmi ${__img} 2>/dev/null
+      done
+    fi
+  done
+  return
+}
+
+__build() {
+  mkdir -p target
+  local __failed
+  for __distro in ${COOPR_DISTRIBUTIONS}; do
+    # Create image
+    __image=omnibus-coopr-${__distro}
+    docker build -f Dockerfile-${__distro} -t ${__image} .
+    if [[ $? -eq 0 ]]; then
+      docker run -ti ${__image} -v $(pwd -P)/target:/tmp/target /bin/bash -c 'cp -f /var/tmp/coopr-build/pkg/* /tmp/target'
+      if [[ $? -eq 0 ]]; then
+        echo "Copied packages to target successfully"
+        return
+      else
+        die "Copying packages failed!"
+      fi
+    else
+      __failed+="${__distro} "
+    fi
+  done
+  if [[ -n ${__failed} ]]; then
+    die "Failed to build the following distribution packages: ${__failed}"
+  fi
+  return
+}
+
+__ret=1
+if [[ ${#} -eq 0 ]]; then
+  echo "No arguments given, running: clean build"
+  __clean && __build
+  __ret=$?
+else
+  while [[ ${#} -gt 0 ]]; then
+    case ${1} in
+      clean|build) __action=${1}; shift; __${__action}; __ret=$? ;;
+      *) die "Unrecognized parameter!" ;;
+    esac
+  done
+fi
+
+exit ${__ret}
